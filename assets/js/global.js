b0ed78b2faad7a93cc614f7f9674c411dd147cfa
hekichien <chienlx1005@gmail.com>
2023-12-09 17:28:21 +0700

    update performance section

--- assets/js/global.js
+++ assets/js/global.js
@@ -240,6 +240,63 @@ class customVideo extends HTMLElement {
 
 }
 customElements.define('custom-video', customVideo)
+// ============================
+// Text typing
+// ============================
+class textTyping extends HTMLElement {
+  constructor() {
+    super();
+    // Initialize variables
+    this.typeJsText = this.querySelector(".animatedText");
+    this.stringIndex = 0; // Index of the current string in the array
+    this.charIndex = 0; // Index of the current character in the current string
+    this.isTyping = true; // Whether we are currently typing or erasing
+    // Define an array of strings to be displayed and erased
+    this.textArray = [
+      "Hello!",
+      "Product customers want",
+      "Kalles landing x EcomRise"
+      // Add more strings as needed
+    ];
+    // Set an interval to call the typeJs function
+    setInterval(() => {
+      this.typeJs();
+    }, 100); // You can adjust the animation speed as needed
+  }
+  typeJs() {
+    if (this.stringIndex < this.textArray?.length) {
+      // Check if there are more strings to display or erase
+      const currentString = this.textArray[this.stringIndex];
+
+      if (this.isTyping) {
+        // Typing animation
+        if (this.charIndex < currentString.length) {
+          this.typeJsText.innerHTML += currentString.charAt(this.charIndex);
+          this.charIndex++;
+        } else {
+          this.isTyping = false; // Switch to erasing mode
+        }
+      } else {
+        // Erasing animation
+        if (this.charIndex > 0) {
+          this.typeJsText.innerHTML = currentString.substring(0, this.charIndex - 1);
+          this.charIndex--;
+        } else {
+          this.isTyping = true; // Switch back to typing mode
+          this.stringIndex++; // Move to the next string
+
+          if (this.stringIndex >= this.textArray.length) {
+            this.stringIndex = 0; // Reset to the beginning of the array
+          }
+
+          this.charIndex = 0; // Reset character index
+          this.typeJsText.innerHTML = ""; // Clear the content for the new string
+        }
+      }
+    }
+  }
+}
+customElements.define('text-typing', textTyping)
 
 
 const revealOptions = {
@@ -280,7 +337,7 @@ const featured = () => {
   }
   const featured_wrapObserver = new IntersectionObserver((entries, observe) => {
     entries.forEach(entry => {
-      console.log(entry);
+      // console.log(entry);
 
     })
   }, featured_wrapOptions)
