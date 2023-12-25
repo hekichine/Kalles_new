cf242bc46371bc616a25544568a45152bf688762
hekichien <chienlx1005@gmail.com>
2023-12-25 14:24:19 +0700

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,11 +22,11 @@ $(document).on('DOMContentLoaded', function() {
   let topbar = new Swiper(".topbar .swiper", swiperOptions);
 
   // active link
-  $(document).on('click', '.header .nav_link', function() {
-    console.log(1);
-    $('.header').find('.nav_link.active').removeClass('active');
-    $(this).addClass('active')
-  })
+  // $(document).on('click', '.header .nav_link', function() {
+  //   console.log(1);
+  //   $('.header').find('.nav_link.active').removeClass('active');
+  //   $(this).addClass('active')
+  // })
 
   // // active link on mobile
   // $(document).on('click', '#menu_mobile .nav_link', function() {
